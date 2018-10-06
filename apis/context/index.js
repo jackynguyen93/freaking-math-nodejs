9535c59ba8268120de277aa22c0100afedbff25e
starfall_9000 <starfall.9000.21@gmail.com>
2018-10-06 18:08:29 +0700

    add challenge api

--- apis/context/index.js
+++ apis/context/index.js
@@ -150,4 +150,60 @@ module.exports = function addContextApi(app) {
       else res.json(formatResponse(errorCode.UNKNOWN))
     }
   })
+
+  app.post('/v1/context/challenge', async (req, res) => {
+    let body = req.body
+
+    try {
+      if (!body) throw errorCode.WRONG_API
+
+      const { contextID, opponentID } = body
+      if (!contextID || !opponentID) throw errorCode.WRONG_API
+
+      const listValue = [
+        opponentID,
+        null,
+        null,
+        null,
+        Date.now() / 1000,
+        contextID
+      ]
+      const insertValues = query.joinListValue(listValue)
+
+      const updateValue = query.joinFieldValue(
+        ['challenge_context'],
+        [contextID]
+      )
+      const updateCondition = query.joinFieldValue(
+        ['player_id'],
+        [opponentID],
+        ' AND '
+      )
+      const updateQuery = query.update('player', updateValue, updateCondition)
+
+      try {
+        await client.connect()
+        const result = await client.transactionQuery([updateQuery])
+
+        if (result.rowCount === 0) {
+          const insertQuery = query.insertInto(
+            'player',
+            undefined,
+            insertValues
+          )
+          await client.transactionQuery([insertQuery])
+        }
+
+        res.json(formatResponse(errorCode.SUCCESS))
+        client.release()
+      } catch (error) {
+        console.error(error)
+        throw errorCode.DB_ERROR
+      }
+    } catch (error) {
+      console.error(error)
+      if (errorCode.hasOwnProperty(error)) res.json(formatResponse(error))
+      else res.json(formatResponse(errorCode.UNKNOWN))
+    }
+  })
 }
